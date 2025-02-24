8afc610932a004abdcbacf773bce587772a1e943
smsterry <smsterry@kaist.ac.kr>
2025-02-24 23:59:13 +0900

    fixed typo

--- PE.v
+++ PE.v
@@ -2,7 +2,7 @@
 //----------------------------------------------------------------------//
 module PE #(
     parameter   OPND_BWIDTH = 8,    // INT8 operands
-    parameter   ACC_BWIDTH  = 32,   // INT32 partial sums
+    parameter   ACC_BWIDTH  = 32    // INT32 partial sums
 ) 
 (
     // Control inputs
@@ -16,18 +16,18 @@ module PE #(
     input wire  OPND2_is_valid_in,  // if 2nd input operand is valid or not
 
     // Data inputs
-    input signed wire [OPND_BWIDTH-1:0] OPND1_in,   // 1st operand from another PE
-    input signed wire [OPND_BWIDTH-1:0] OPND2_in,   // 2nd operand from another PE
-    input signed wire [ACC_BWIDTH-1:0]  ACC_in      // Accumulated partial sum (for flushing)
+    input wire signed [OPND_BWIDTH-1:0] OPND1_in,   // 1st operand from another PE
+    input wire signed [OPND_BWIDTH-1:0] OPND2_in,   // 2nd operand from another PE
+    input wire signed [ACC_BWIDTH-1:0]  ACC_in,     // Accumulated partial sum (for flushing)
 
     // Control outputs
     output wire OPND1_is_valid_out, // if 1st output operand is valid or not
     output wire OPND2_is_valid_out, // if 2nd output operand is valid or not
 
     // Data outputs
-    output signed wire [OPND_BWIDTH-1:0]    OPND1_out,  // 1st operand to another PE
-    output signed wire [OPND_BWIDTH-1:0]    OPND2_out,  // 2nd operand to another PE
-    output signed wire [ACC_BWIDTH-1:0]     ACC_out     // Accumulated partial sum (for flushing)
+    output wire signed [OPND_BWIDTH-1:0]    OPND1_out,  // 1st operand to another PE
+    output wire signed [OPND_BWIDTH-1:0]    OPND2_out,  // 2nd operand to another PE
+    output wire signed [ACC_BWIDTH-1:0]     ACC_out     // Accumulated partial sum (for flushing)
 );
 
 // Buffers for the operands and the accumulated partial sum
@@ -46,7 +46,7 @@ wire signed [ACC_BWIDTH-1:0]    product;
 
 // Combinational computing logic
 assign curr_acc = acc_buf;
-assign product  = opnd1_buf * opnd2_buf
+assign product  = opnd1_buf * opnd2_buf;
 assign acc_nxt  = curr_acc + product;
 
 // Combinational output logic: output wires that will be used by other PEs
